e73897b83bbc6b85a2c9506a68a67a8b20e18777
Jean-Christophe Bourgault <jcbourgault2008@yahoo.ca>
2015-05-30 10:04:07 -0400

    Commented start

--- Bank/start.lua
+++ Bank/start.lua
@@ -4,7 +4,7 @@ while stop==false
 	do
 	interest=count/76800 --for each stack, earn 1 item each in-game day (20 minutes)
 	count=count+interest
-	if redstone.getInput(top)
+	if redstone.getInput(top) --can be set to whatever side your stop switch will be
 		then
 		stop=true
 	end
